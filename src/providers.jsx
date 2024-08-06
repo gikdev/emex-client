5c8b41090810946f0a92a64212bf1550c6be77ec
gikdev <m.m.bhrm85@gmail.com>
2024-08-06 18:32:05 +0330

    Added singalR indicator

--- src/providers.jsx
+++ src/providers.jsx
@@ -1,12 +1,13 @@
 import { version } from "@/assets/data.json"
 import { VersionTag } from "@/components"
 import { Toaster } from "react-hot-toast"
+import { SignalRProvider } from "./contexts"
 
 function Providers({ children }) {
   return (
     <>
       <Toaster toastOptions={{ position: "bottom-left" }} />
-      {children}
+      <SignalRProvider>{children}</SignalRProvider>
       <VersionTag version={version} />
     </>
   )
