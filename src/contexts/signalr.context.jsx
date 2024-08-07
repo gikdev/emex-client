1e4a88c862d6d02dcdafb7490b2b8338059b8bde
gikdev <m.m.bhrm85@gmail.com>
2024-08-07 12:11:10 +0330

    fix(app): Fixed SignalR connection, and added auto-reconnect

--- src/contexts/signalr.context.jsx
+++ src/contexts/signalr.context.jsx
@@ -6,15 +6,16 @@ const SignalRContext = createContext()
 
 const useSignalRContext = () => useContext(SignalRContext)
 
+const OK_CONNECTION_STATES = ["connected", "loading"]
+
 function SignalRProvider({ children }) {
-  // Available states: "disconnected" | "connected" | "loading" | "uknown"
+  // Available states: "disconnected" | "connected" | "uknown"
   const [connectionState, setConnectionState] = useState()
   const connectionRef = useRef(null)
 
   const value = { connectionRef, connectionState }
 
   const handleClose = useCallback(() => setConnectionState("disconnected"), [])
-  const handleReconnecting = useCallback(() => setConnectionState("loading"), [])
   const handleReconnected = useCallback(() => setConnectionState("connected"), [])
   const handleStarted = useCallback(() => setConnectionState("connected"), [])
   const handleConnectionFailed = useCallback(() => setConnectionState("disconnected"), [])
@@ -24,17 +25,29 @@ function SignalRProvider({ children }) {
     const newConnection = new signalR.HubConnectionBuilder()
       .withUrl(`${apiHelper.BASE_URL}/priceHub`)
       .build()
-    newConnection.onreconnecting(handleReconnecting)
     newConnection.onreconnected(handleReconnected)
     newConnection.onclose(handleClose)
     newConnection.start().then(handleStarted).catch(handleConnectionFailed)
+    connectionRef.current = newConnection
 
-    function cleanup() {
-      connectionRef.current?.stop()
-    }
+    const cleanup = () => connectionRef.current?.stop()
 
     return cleanup
   }, [])
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const isOKState = OK_CONNECTION_STATES.includes(connectionState)
+      if (isOKState) return
+
+      connectionRef.current?.start().then(handleStarted).catch(handleConnectionFailed)
+    }, 3000)
+
+    const cleanup = () => clearInterval(interval)
+
+    return cleanup
+  }, [connectionState])
 
   return <SignalRContext.Provider value={value}>{children}</SignalRContext.Provider>
 }
