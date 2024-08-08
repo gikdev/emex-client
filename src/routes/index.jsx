0c6598fa708c3ad7c9ca397dbc2403751cb7816a
gikdev <m.m.bhrm85@gmail.com>
2024-08-08 17:44:08 +0330

    Enabled `/orders` in the menu...

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "wouter"
 import { About } from "./about/about.route"
 import { Home } from "./home/home.route"
 import { Login } from "./login/login.route"
+import { Orders } from "./orders/orders.route"
 import { Profile } from "./profile/profile.route"
 import { Rules } from "./rules/rules.route"
 import { Test } from "./test.route"
@@ -17,6 +18,7 @@ function Routes() {
           <Route path="/" component={Home} />
           <Route path="/trade" component={Trade} />
           <Route path="/profile" component={Profile} />
+          <Route path="/orders" component={Orders} />
           <Route path="/about" component={About} />
           <Route path="/rules" component={Rules} />
           <Route path="/test" component={Test} />
