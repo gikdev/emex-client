84e1f273f8ead7cc48160e49b27e2b3d29aee5a1
gikdev <m.m.bhrm85@gmail.com>
2024-08-09 12:38:57 +0330

    Made the remainings page...

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,15 @@ import { PersianDate, addCommaToPrice, persianifyNumber, priceToRial, priceToTom
 
 const formatters = {
   date: p => new PersianDate(p.value).toLocaleDateString(),
+  debt: p => {
+    const { value } = p
+
+    const volume = value < 0 ? value * -1 : value
+    const volumeToShow = priceToRial(volume, 4)
+    console.log({ value, volume, volumeToShow })
+
+    return `${volumeToShow} ${value < 0 ? "(بدهکار)" : "(بستانکار)"}`
+  },
   orderSide: p => enumToText("ORDER_SIDE", p.value),
   orderStatus: p => enumToText("ORDER_STATUS", p.value),
   persianComma: p => addCommaToPrice(persianifyNumber(p.value)),
--- src/routes/remainings/remainings.route.jsx
+++ src/routes/remainings/remainings.route.jsx
@@ -1,5 +1,18 @@
+import { Heading, Hr } from "@/components"
+import { RemainingsTable } from "./remainings-table.component"
+
 function Remainings() {
-  return "Remaingings"
+  return (
+    <>
+      <Heading as="h1" size={5} className="text-slatedark-12 mb-4 mt-6 text-center">
+        مانده حساب
+      </Heading>
+      <Hr />
+      <section className="h-full p-8">
+        <RemainingsTable />
+      </section>
+    </>
+  )
 }
 
 export { Remainings }
