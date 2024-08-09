c4c289491d467f8e8f01c587898b5733028488be
gikdev <m.m.bhrm85@gmail.com>
2024-08-09 17:42:22 +0330

    Made the update banner also

--- src/providers.jsx
+++ src/providers.jsx
@@ -1,9 +1,12 @@
 import { version } from "@/assets/data.json"
 import { VersionTag } from "@/components"
+import { useHandleUpdate } from "@/hooks"
 import { Toaster } from "react-hot-toast"
 import { SignalRProvider } from "./contexts"
 
 function Providers({ children }) {
+  useHandleUpdate(version)
+
   return (
     <>
       <Toaster toastOptions={{ position: "bottom-left" }} />
